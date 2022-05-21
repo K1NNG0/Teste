5eb6fe78e997b8f8acaa817b2df06358f0ef5cdf
K1NNG0 <103907641+K1NNG0@users.noreply.github.com>
2022-05-21 03:17:58 -0300

    Update Phone.bat

--- Phone.bat
+++ Phone.bat
@@ -57,14 +57,62 @@ call lay
 call Button 9 4 "Itens Recentes" 20 0 "%date%" # Info
 call Button 1 7 "%histapp1%" 13 7 "%histapp2%" 25 7 "%histapp3%" 21 23 "App Store" 1 0 "%web%" 10 27 "" 15 27 "" 20 27 "" # Press
 Getinput /m %Press% /h 70
-if %errorlevel%==1 goto histapp
-if %errorlevel%==2 goto histapp
-if %errorlevel%==3 goto histapp
+if %errorlevel%==1 goto olapps1
+if %errorlevel%==2 goto olapps2
+if %errorlevel%==3 goto olapps3
 if %errorlevel%==4 goto store0
 if %errorlevel%==5 call cnx
 if %errorlevel%==6 goto %pg%
 if %errorlevel%==7 goto home
 if %errorlevel%==8 goto home
+goto lapps
+
+:olapps1
+if %histapp1%==Nada goto nireslapps
+cls
+cd %listapp%
+if %histapp1%==%app1% call %app1%
+if %histapp1%==%app2% call %app2%
+if %histapp1%==%app3% call %app3%
+if %histapp1%==%app4% call %app4%
+if %histapp1%==%app5% call %app5%
+if %histapp1%==%app6% call %app6%
+if %histapp1%==%app7% call %app7%
+if %histapp1%==%app8% call %app8%
+if %histapp1%==%app9% call %app9%
+cd %home%
+goto lapps
+
+:olapps2
+if %histapp2%==Nada goto nireslapps
+cls
+cd %listapp%
+if %histapp2%==%app1% call %app1%
+if %histapp2%==%app2% call %app2%
+if %histapp2%==%app3% call %app3%
+if %histapp2%==%app4% call %app4%
+if %histapp2%==%app5% call %app5%
+if %histapp2%==%app6% call %app6%
+if %histapp2%==%app7% call %app7%
+if %histapp2%==%app8% call %app8%
+if %histapp2%==%app9% call %app9%
+cd %home%
+goto lapps
+
+:olapps3
+if %histapp3%==Nada goto nireslapps
+cls
+cd %listapp%
+if %histapp3%==%app1% call %app1%
+if %histapp3%==%app2% call %app2%
+if %histapp3%==%app3% call %app3%
+if %histapp3%==%app4% call %app4%
+if %histapp3%==%app5% call %app5%
+if %histapp3%==%app6% call %app6%
+if %histapp3%==%app7% call %app7%
+if %histapp3%==%app8% call %app8%
+if %histapp3%==%app9% call %app9%
+cd %home%
 goto lapps
 
 :store0
@@ -95,8 +143,7 @@ if %errorlevel%==3 goto home
 if %errorlevel%==4 goto lapps
 goto store0error
 
-::checar historico de apps antes de mandar pro ":histapp"
-:histapp
+:nireslapps
 call cnx
 cls
 call lay
@@ -107,7 +154,7 @@ if %errorlevel%==1 call cnx
 if %errorlevel%==2 goto lapps
 if %errorlevel%==3 goto home
 if %errorlevel%==4 goto home
-goto histapp
+goto nireslapps
 
 ::teste de apps
 :apptest
@@ -118,17 +165,19 @@ if %lapp%==0 goto home
 goto home
 
 :app
+set histapp3=%histapp2%
+set histapp2=%histapp1%
 cls
 cd %listapp%
-if %oapp%==1 call %app1%
-if %oapp%==2 call %app2%
-if %oapp%==3 call %app3%
-if %oapp%==4 call %app4%
-if %oapp%==5 call %app5%
-if %oapp%==6 call %app6%
-if %oapp%==7 call %app7%
-if %oapp%==8 call %app8%
-if %oapp%==9 call %app9%
+if %oapp%==1 set histapp1=%app1% && call %app1%
+if %oapp%==2 set histapp1=%app2% && call %app2%
+if %oapp%==3 set histapp1=%app3% && call %app3%
+if %oapp%==4 set histapp1=%app4% && call %app4%
+if %oapp%==5 set histapp1=%app5% && call %app5%
+if %oapp%==6 set histapp1=%app6% && call %app6%
+if %oapp%==7 set histapp1=%app7% && call %app7%
+if %oapp%==8 set histapp1=%app8% && call %app8%
+if %oapp%==9 set histapp1=%app9% && call %app9%
 if %lapp%==1 set lapp=0 && goto lapps
 cd %home%
 if %lapp%==0 goto home
